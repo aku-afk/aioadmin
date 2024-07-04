c8e2643137e4df34b0fbe0358210d87eadf39d55
aku-afk <mamat@mmtprjct.id>
2024-07-05 00:37:53 +0700

    .

--- index.php
+++ index.php
@@ -25,7 +25,6 @@ function scnDir($dst) {
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PAPE-PAPE</title>
 </head>
 <style>
