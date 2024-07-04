cb0788f768f6f57f1b2e4817d5810c599f081f35
aku-afk <mamat@mmtprjct.id>
2024-07-05 00:08:09 +0700

    .

--- index.php
+++ index.php
@@ -1,11 +1,38 @@
 <?php
 
-$scn = scandir('/');
+function scnDir($dst) {
+    $res = scandir($dst);
+    return $res;
+}
 
 ?>
 
-<pre>
-    <?php
-        print_r($scn);
-    ?>
-</pre>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PAPE-PAPE</title>
+</head>
+<body>
+    <table>
+        <thead>
+            <td>name</td>
+        </thead>
+        <tbody>
+            <?php
+                $scandir = scnDir('/');
+                foreach ($scandir as $key => $value) {
+             ?>
+             <tr>
+                <td>
+                    <?php echo $value; ?>
+                </td>
+             </tr>
+             <?php
+                }
+            ?>
+        </tbody>
+    </table>
+</body>
+</html>
